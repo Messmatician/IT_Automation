6faf164faf462f64dfb1222d293d8eeb975f7c27
Ian <ian.younanc@gmail.com>
2020-10-27 11:21:01 +0000

    jpeg converter

---
+++ changeImage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from PIL import Image
+import os
+import os.path
+
+dir = "/home/student-04-39b239d0c098/supplier-data/images"
+for image in os.listdir(dir):
+  if ".tiff" in image:
+    img = Image.open(dir + "/" + image)
+    imgname = image.strip(".tiff")
+    img.resize((600, 400)).convert("RGB").save(dir + "/" + imgname + ".jpeg", "JPEG")
