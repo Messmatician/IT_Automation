6de55442c4b20c1de08737abce7e0b2724a78292
Ian <ian.younanc@gmail.com>
2020-10-27 11:29:48 +0000

    made some changes

--- changeImage.py
+++ changeImage.py
@@ -5,7 +5,7 @@ import os.path
 
 dir = "/home/student-04-39b239d0c098/supplier-data/images"
 for image in os.listdir(dir):
-  if ".tiff" in image:
+  if image.endswith(".tiff"):
     img = Image.open(dir + "/" + image)
     imgname = image.strip(".tiff")
     img.resize((600, 400)).convert("RGB").save(dir + "/" + imgname + ".jpeg", "JPEG")
